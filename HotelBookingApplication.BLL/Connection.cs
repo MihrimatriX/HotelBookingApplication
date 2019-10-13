9e18023962bcf56476ed34d798c3c9143d2919a1
caglarbingol <bingolcaglar@gmail.com>
2019-10-13 18:56:28 +0300

    Çağlar - DLL eklendi

--- HotelBookingApplication.BLL/Connection.cs
+++ HotelBookingApplication.BLL/Connection.cs
@@ -20,12 +20,66 @@ namespace HotelBookingApplication.BLL
             }
             return true;
         }
-        public bool IsThisAlreadyCustomer()
+        public int TcNoSearch(string tcNo)
         {
-            foreach(Customer item db.Customers.ToList())
+            foreach (Customer item in db.Customers.ToList())
             {
-                if (item.TCNo == )
+                if (item.TCNo == tcNo)
+                {
+                    return item.CustomerID;
+                }
+            }
+            return -1;
+        }
+        public bool IsAppUser(int customerId)
+        {
+            foreach (AppUser item in db.AppUsers.ToList())
+            {
+                if (item.CustomerID == customerId)
+                    return true;
+            }
+            return false;
+        }
+        public void AddUser(int customerId, string username, string password)
+        {
+            AppUser appUser = new AppUser()
+            {
+                CustomerID = customerId,
+                UserName = username,
+                Password = password
+            };
+            db.AppUsers.Add(appUser);
+            db.SaveChanges();
+        }
+        public void AddRoom(int numberOfRoom)
+        {
+            if (db.Rooms.ToList().Count != 20)
+            {
+                for (int i = db.Rooms.ToList().Count; i < numberOfRoom; i++)
+                {
+                    Room room = new Room();
+                    db.Rooms.Add(room);
+                }
             }
         }
+        public void AddReservation(DateTime arrivalDate, DateTime departureDate, int optionId, int customerId, int numberOfCustomers)
+        {
+            Reservation reservation = new Reservation()
+            {
+                ArrivalDate = arrivalDate,
+                DepartureDate = departureDate,
+                OptionID = optionId,
+                CustomerID = customerId,
+                Price = CalculatePrice(numberOfCustomers,optionId)
+
+            };
+            db.Reservations.Add(reservation);
+        }
+
+        private decimal CalculatePrice(int numberOfCustomer,int optionId)
+        {
+        }
+
     }
+
 }
