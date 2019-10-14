475046fc2739c63613858c3b36b67047e6a44f90
caglarbingol <bingolcaglar@gmail.com>
2019-10-14 11:22:53 +0300

    Add UI

--- HotelBookingApplication.UI/NewUserInterface.Designer.cs
+++ HotelBookingApplication.UI/NewUserInterface.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewUserInterface));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -40,68 +41,83 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(193, 103);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(150, 297);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.Size = new System.Drawing.Size(34, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Ad:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(176, 136);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(133, 334);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.Size = new System.Drawing.Size(57, 18);
             this.label2.TabIndex = 1;
             this.label2.Text = "Soyad:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(175, 169);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(132, 367);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.Size = new System.Drawing.Size(61, 18);
             this.label3.TabIndex = 2;
             this.label3.Text = "TC No:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(149, 202);
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(384, 334);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.Size = new System.Drawing.Size(111, 18);
             this.label4.TabIndex = 3;
             this.label4.Text = "Kullanıcı Adı:";
             // 
             // txtFirstName
             // 
-            this.txtFirstName.Location = new System.Drawing.Point(222, 100);
+            this.txtFirstName.BackColor = System.Drawing.Color.Tan;
+            this.txtFirstName.Location = new System.Drawing.Point(196, 294);
             this.txtFirstName.Name = "txtFirstName";
             this.txtFirstName.Size = new System.Drawing.Size(167, 20);
             this.txtFirstName.TabIndex = 4;
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(222, 133);
+            this.txtLastName.BackColor = System.Drawing.Color.Tan;
+            this.txtLastName.Location = new System.Drawing.Point(196, 329);
             this.txtLastName.Name = "txtLastName";
             this.txtLastName.Size = new System.Drawing.Size(167, 20);
             this.txtLastName.TabIndex = 5;
             // 
             // txtTcNo
             // 
-            this.txtTcNo.Location = new System.Drawing.Point(222, 166);
+            this.txtTcNo.BackColor = System.Drawing.Color.Tan;
+            this.txtTcNo.Location = new System.Drawing.Point(196, 364);
             this.txtTcNo.Name = "txtTcNo";
             this.txtTcNo.Size = new System.Drawing.Size(167, 20);
             this.txtTcNo.TabIndex = 6;
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(222, 199);
+            this.txtUserName.AllowDrop = true;
+            this.txtUserName.BackColor = System.Drawing.Color.Tan;
+            this.txtUserName.Location = new System.Drawing.Point(501, 334);
             this.txtUserName.Name = "txtUserName";
             this.txtUserName.Size = new System.Drawing.Size(167, 20);
             this.txtUserName.TabIndex = 7;
@@ -109,43 +125,64 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(176, 235);
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(426, 370);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.Size = new System.Drawing.Size(60, 18);
             this.label5.TabIndex = 8;
             this.label5.Text = "Parola:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(222, 232);
+            this.textBox1.BackColor = System.Drawing.Color.Tan;
+            this.textBox1.Location = new System.Drawing.Point(501, 367);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(167, 20);
             this.textBox1.TabIndex = 9;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(222, 268);
+            this.btnSave.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnSave.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Bold);
+            this.btnSave.Location = new System.Drawing.Point(278, 408);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.Size = new System.Drawing.Size(85, 30);
             this.btnSave.TabIndex = 10;
             this.btnSave.Text = "KAYDET";
-            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.UseVisualStyleBackColor = false;
             this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(314, 268);
+            this.btnCancel.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnCancel.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Bold);
+            this.btnCancel.Location = new System.Drawing.Point(501, 408);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(85, 30);
             this.btnCancel.TabIndex = 11;
             this.btnCancel.Text = "İPTAL";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(798, 277);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
             // 
             // NewUserInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.textBox1);
@@ -160,6 +197,7 @@
             this.Controls.Add(this.label1);
             this.Name = "NewUserInterface";
             this.Text = "NewUserInterface";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -179,5 +217,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
